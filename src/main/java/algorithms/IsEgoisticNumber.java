465741d52d075bcb088ea09e10d8240f46955ba1
rrmaduzia <rafi11m@hotmail.com>
2020-11-26 22:54:11 +0100

    Initial commit

---
+++ src/main/java/algorithms/IsEgoisticNumber.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+public class IsEgoisticNumber {
+
+
+    public static boolean execute(int number) {
+
+        int pow = number * number;
+        int lastNumber = pow %10 ;
+
+        return lastNumber == number;
+
+    }
+
+
+}
