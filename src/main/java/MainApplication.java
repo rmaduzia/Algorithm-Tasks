147b1cd72f2e3180baa7c02e2165017608e0b253
rrmaduzia <rafi11m@hotmail.com>
2020-12-01 14:03:01 +0100

    adventOfCodeDay1

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,11 +1,13 @@
+import algorithms.adventOfCode.AdventDay1;
 
-
+import java.io.IOException;
 
 public class MainApplication {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException {
         System.out.println("Algorytmy");
 
+        int AdventDay1Result = AdventDay1.readDateFromFile();
 
     }
 }
