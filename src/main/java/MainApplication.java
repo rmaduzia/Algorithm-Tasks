5cbd78868b876efaab5c57e58f3b3fcc66aebfc8
rrmaduzia <rafi11m@hotmail.com>
2021-01-15 15:04:25 +0100

    cleaned code

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -9,21 +9,13 @@ public class MainApplication {
     public static void main(String[] args) throws IOException {
         System.out.println("Algorytmy");
 
-        int AdventDay1Result = AdventDay1.getResultAdventDay1();
-        int AdventDay2Part1Result = AdventDay2.getResultAdventDay2Part1();
-        int AdventDay2Part2Result = AdventDay2.getResultAdventDay2Part2();
-        int AdventDay3Part1Result = AdventDay3.getResultAdventDay3Part1(3, 1);
-        long AdventDay3Part2Result = AdventDay3.getResultAdventDay3Part2();
-        int AdventDay4ResultPart1 = AdventDay4.getResultAdventDay4Part1();
-        int AdventDay4ResultPart2 = AdventDay4.getResultAdventDay4Part2();
-        int AdventDay5ResultPart1 = AdventDay5.getResultAdventDay5Part1();
+
         int AdventDay6ResultPart1 = AdventDay6.getResultAdventDay6Part1();
 
         int AdventDay6ResultPart2 = AdventDay6.getResultAdventDay6Part2();
 
         System.out.println(AdventDay6ResultPart2);
 
-        // System.out.println(PrimeGenerator.execute(1L,10L));
 
 
     }
