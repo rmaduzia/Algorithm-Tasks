05ffc613161f967d25f3e414948463df0cdc4f93
rrmaduzia <rafi11m@hotmail.com>
2021-06-26 12:51:53 +0200

    adventOfCode Day7

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,22 +1,15 @@
 import algorithms.adventOfCode.*;
-import algorithms.spoj.green.PrimeGenerator;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class MainApplication {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Algorytmy");
 
-
-        int AdventDay6ResultPart1 = AdventDay6.getResultAdventDay6Part1();
-
-        int AdventDay6ResultPart2 = AdventDay6.getResultAdventDay6Part2();
-
-        System.out.println(AdventDay6ResultPart2);
-
-
+        AdventDay7 AdventDay7 = new AdventDay7();
+        int AdventDay7Result = AdventDay7.getResultAdventDay7Part2();
+        System.out.println(AdventDay7Result);
 
     }
 }
